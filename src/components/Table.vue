3f32426c18c56a585265f660cc5de889102fae41
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-06-05 22:34:54 +0200

    Updated classes in Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,16 +1,16 @@
 <template>
   <table class="table" cellspacing="0" cellpadding="0">
-    <tr>
-      <th>Klient</th>
-      <th>Produkt</th>
-      <th>Kwota</th>
-      <th>Wykorzystany rabat</th>
+    <tr class="table__header">
+      <th class="table__heading">Klient</th>
+      <th class="table__heading">Produkt</th>
+      <th class="table__heading">Kwota</th>
+      <th class="table__heading">Wykorzystany rabat</th>
     </tr>
-    <tr v-for="(row, index) in data" :key="index">
-      <td>{{row.client}}</td>
-      <td>{{row.product}}</td>
-      <td>{{row.cost}} $</td>
-      <td>{{row.rabat}} %</td>
+    <tr class="table__row" v-for="(row, index) in data" :key="index">
+      <td class="table__data">{{row.client}}</td>
+      <td class="table__data">{{row.product}}</td>
+      <td class="table__data">{{row.cost}} $</td>
+      <td class="table__data">{{row.rabat}} %</td>
     </tr>
   </table>
 </template>
@@ -24,37 +24,54 @@ export default {
 
 <style lang="scss">
 .table {
-
   border: 0;
-  tr {
-    cursor: pointer;
-    transition: box-shadow 200ms;
+}
 
-    &:nth-child(2n) {
-      background-color: #fafafa;
-    }
-    &:not(:first-child):hover {
-      box-shadow: 1.5px 2.598px 16px rgba(0, 0, 1, 0.1);
-      td:first-child::before {
-        opacity: 1;
-      }
-    }
-  }
-  th {
-    text-align: left;
-    text-transform: uppercase;
-    font-size: 13px;
-    color: #a1a1a1;
-    font-family: "Proxima Nova";
-  }
-  td {
-    font-size: 14px;
-    color: #000000;
-    font-family: "Proxima Nova";
-  }
-  td:first-child,
-  th:first-child {
+.table__header {
+  text-align: left;
+  text-transform: uppercase;
+  font-size: 13px;
+  color: #a1a1a1;
+  font-family: "Proxima Nova";
+}
+
+.table__heading {
+  &:first-child {
     padding-left: 50px;
+    padding-top: 25px;
+    padding-bottom: 25px;
+  }
+  &:not(:first-child) {
+    padding-top: 25px;
+    padding-bottom: 25px;
+    padding-right: 50px;
+  }
+}
+
+.table__row {
+  cursor: pointer;
+  transition: box-shadow 200ms;
+
+  &:nth-child(2n) {
+    background-color: #fafafa;
+  }
+  &:hover {
+    box-shadow: 1.5px 2.598px 16px rgba(0, 0, 1, 0.1);
+    .table__data:first-child::before {
+      opacity: 1;
+    }
+  }
+}
+
+.table__data {
+  font-size: 14px;
+  color: #000000;
+  font-family: "Proxima Nova";
+
+  &:first-child {
+    padding-left: 50px;
+    padding-top: 25px;
+    padding-bottom: 25px;
     position: relative;
     &::before {
       content: "";
@@ -70,8 +87,8 @@ export default {
       transition: opacity 200ms;
     }
   }
-  td:not(:first-child),
-  th:not(:first-child) {
+
+  &:not(:first-child) {
     padding-top: 25px;
     padding-bottom: 25px;
     padding-right: 50px;
